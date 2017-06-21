09416f35630a2f784de64814b7bb5f53d7eb399a
TREINAR <TREINAR@MICRO03>
2017-06-21 19:14:26 -0300

    Meu projetop em andamento

---
+++ projeto/treinar-web/src/br/com/treinar/controller/HelloWorld.java
@@ -0,0 +1,38 @@
+package br.com.treinar.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(urlPatterns = "*.trn")
+public class HelloWorld extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public HelloWorld(){
+		System.out.println("iniciou...");
+	}
+	
+	private String message;
+
+	public void init() throws ServletException {
+		// Do required initialization
+		message = "Ola Mundo [${nome}]";
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Set response content type
+		response.setContentType("text/html");
+
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		String nome = request.getParameter("nome");
+		out.println("<h1>" + message.replace("${nome}", nome) + "</h1>");
+	}
+
+	public void destroy() {
+		// do nothing.
+	}
+}
